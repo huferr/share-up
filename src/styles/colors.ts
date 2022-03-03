ab97555bad8ed1a5297ac77740cdb593cc8490b8
Hugo Ferreira <hugoferreirait@gmail.com>
2022-03-03 20:27:18 -0300

    feat: adding default colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#1D9BF0",
+  danger: "#FF1616",
+  warning: "#EFCA08",
+  darkerBlue: "#001B2D",
+  grayBlue: "#4F738B",
+  green: "#24FF00",
+  lightRed: "#E32727",
+  gray: "#C4C4C4",
+} 
